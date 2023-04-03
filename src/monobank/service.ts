03aa0d827d94946c373ebae399651af74cfec05a
nikdnestr <nikita.dnestrov@gmail.com>
2023-04-03 23:52:44 +0300

    fix test

--- src/monobank/service.ts
+++ src/monobank/service.ts
@@ -3,23 +3,26 @@ import { transactionsRef } from "../db";
 import { Transaction } from "@prisma/client";
 
 class MonobankService {
+  //   async test() {
+  //     const { data }: AxiosResponse = await axios.get(
+  //       "https://api.monobank.ua/personal/statement/G1m1U4Cxa1AvIE3iYlrFrw/1678044176/1680549776",
+  //       {
+  //         headers: { "X-Token": "ufYVzv7ScuIyS_SGA3XEJnNVLo_8ie7WUg54bv3_h23c" },
+  //       }
+  //     );
+
+  //     const mapped = data.map((el: any) => ({
+  //       id: el.id,
+  //       transactionTime: el.time,
+  //       mcc: el.mcc,
+  //       transactionAmount: el.amount,
+  //     })) as Transaction[];
+
+  //     await transactionsRef.createMany({ data: mapped });
+  //     return data;
+  //   }
   async test() {
-    const { data }: AxiosResponse = await axios.get(
-      "https://api.monobank.ua/personal/statement/G1m1U4Cxa1AvIE3iYlrFrw/1678044176/1680549776",
-      {
-        headers: { "X-Token": "ufYVzv7ScuIyS_SGA3XEJnNVLo_8ie7WUg54bv3_h23c" },
-      }
-    );
-
-    const mapped = data.map((el: any) => ({
-      id: el.id,
-      transactionTime: el.time,
-      mcc: el.mcc,
-      transactionAmount: el.amount,
-    })) as Transaction[];
-
-    await transactionsRef.createMany({ data: mapped });
-    return data;
+    return { hello: "world" };
   }
 }
 
